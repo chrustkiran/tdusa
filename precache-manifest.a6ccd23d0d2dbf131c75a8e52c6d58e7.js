56d6eace24e5483f763363e27874409241308bdc
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2019-12-25 21:13:50 +0530

    Updates

---
+++ precache-manifest.a6ccd23d0d2dbf131c75a8e52c6d58e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b69724a9106aef04f771bbcaf524a4",
+    "url": "/tdusa/index.html"
+  },
+  {
+    "revision": "5a7e64df1fc45ac008e3",
+    "url": "/tdusa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9fc550cc4cad5c564c0d",
+    "url": "/tdusa/static/js/2.95d9dc0f.chunk.js"
+  },
+  {
+    "revision": "1fe50f8c3be9a422a171dfc38257fbe8",
+    "url": "/tdusa/static/js/2.95d9dc0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7e64df1fc45ac008e3",
+    "url": "/tdusa/static/js/main.d9aac918.chunk.js"
+  },
+  {
+    "revision": "dac0254da5a617b9cdd8",
+    "url": "/tdusa/static/js/runtime-main.27631b14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tdusa/static/media/logo.5d5d9eef.svg"
+  }
+]);
