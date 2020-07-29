4c633121061d1e4ab04acdc05a0fc4e05bf744ca
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2020-07-29 11:04:39 +0530

    Updates

---
+++ precache-manifest.d6c99d6743ca6ec0fd1f2d6c243638c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cd5b9e1980f8a5812d6ec95dfb0dcb",
+    "url": "/tdusa/index.html"
+  },
+  {
+    "revision": "c776958c763320510a3f",
+    "url": "/tdusa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9fc550cc4cad5c564c0d",
+    "url": "/tdusa/static/js/2.95d9dc0f.chunk.js"
+  },
+  {
+    "revision": "1fe50f8c3be9a422a171dfc38257fbe8",
+    "url": "/tdusa/static/js/2.95d9dc0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c776958c763320510a3f",
+    "url": "/tdusa/static/js/main.3860ac3b.chunk.js"
+  },
+  {
+    "revision": "dac0254da5a617b9cdd8",
+    "url": "/tdusa/static/js/runtime-main.27631b14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tdusa/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d60e7fbbe6ca6216a0eec580d50bdca.js"
+  "/tdusa/precache-manifest.d6c99d6743ca6ec0fd1f2d6c243638c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tdusa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
